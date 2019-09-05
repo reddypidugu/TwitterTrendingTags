4695558a9a452266088628d740b9ddeda09e84e8
R4519564 <Reddeiah.Pidugu@ext.saint-gobain.com>
2019-09-05 23:06:59 +0200

    added some documentation

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # TwitterTrendingTags
-TwitterTrendingTags: Spark Streaming in a standalone project
+TwitterTrendingTags: Spark Streaming App to consume Twitter Trending Tags
 
-## QUICK START
+## HOW TO START
 
-* install Apache Spark (http://spark.apache.org/):wq
-* install sbt (http://www.scala-sbt.org/)
 * git clone https://github.com/reddypidugu/TwitterTrendingTags.git
 * edit twitter4j.properties or modify main class with proper twitter credentials
-* mvn clean install
-* spark-submit --master --packages org.apache.spark:spark-streaming-twitter_2.10:1.5.1 --jars spar
+* Run the below coammdn command to buildl the appliction jar and docker image.
+```
+mvn clean install
+```
+* The above command will create a docker image at /target as "myimage/TwitterTrendingTags:latest"
+* Docker image contains the following command which will ensure Spark local mode cluster will spin up and application start consuming the tweets tagged with "Amsterdam" 
+  ```
+  spark-submit --master --packages org.apache.spark:spark-streaming-twitter_2.10:1.5.1 --jars spar
   k-core_2.11-1.5.2.logging.jar ./target/twittertrendingtags.jar Amsterdam
+  ```
+* To spin up the Docker image run the following command
+```
+ docker run myimage/TwitterTrendingTags:latest
+``` 
   
-  ## How to Run a Docker Image
+* The hashtags retrieved from the Twitter API stream will be printed on console and also appended to "hashtags.json" file application directory where it is running.  
+
   
 
--- src/main/scala/com/mytweets/TwitterTrendingTags.scala
+++ src/main/scala/com/mytweets/TwitterTrendingTags.scala
@@ -12,7 +12,7 @@ object TwitterTrendingTags  {
   def main(args: Array[String]) {
     val filters = args
 
-    // can use twitter4j.properties
+    // adding Twitter security keys for Twitter API Access
     System.setProperty("twitter4j.oauth.consumerKey", "758I8BkuAvF7l5AUwNqjYYa2f")
     System.setProperty("twitter4j.oauth.consumerSecret", "HpC5puW1KMjI0xQiAtjgsNaXZ3s7Ok40QLK5yebpe5oB5vNx48")
     System.setProperty("twitter4j.oauth.accessToken", "47879036-qYq68GUR0mAlh458tMHdekeVtbb6vx2GC8RUMop8i")
@@ -32,7 +32,7 @@ object TwitterTrendingTags  {
     val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
 
     val topCounts72Hrs = hashTags.map((_, 1))
-                        .reduceByKeyAndWindow(_ + _, Seconds(3 * 86400))
+                        .reduceByKeyAndWindow(_ + _, Seconds(259200)) // to get tags from last 72hrs
                         .map{case (topic, count) => (count, topic)}
                         .transform(_.sortByKey(false))
 
@@ -45,9 +45,11 @@ object TwitterTrendingTags  {
         case (count, tag) =>
           println("tag:%s count:(%s tweets)".format(tag, count))
 
+          //convert string to json string
           val jsonTag = Tweet(tag.toString, count.toString)
           val jsonTagString = jsonTag.toJson.prettyPrint
           println("Pretty String :%s ".format(jsonTagString))
+
           val file = new FileWriter("hashtags.json", true)
             try { //File Writer creates a file in write mode at the given location
               file.write(jsonTagString)
@@ -57,7 +59,6 @@ object TwitterTrendingTags  {
                 e.printStackTrace()
             } finally if (file != null) file.close()
           }
-
     })
 
     ssc.start()
